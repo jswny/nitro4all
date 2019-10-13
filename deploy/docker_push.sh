4d753d7d6659bce4cd3cbfecb8f3a4944833d280
Joe Sweeney <joe@j1.io>
2019-10-13 18:17:24 -0400

    Fix Docker push script

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -2,5 +2,5 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build --tag "$REPOSITORY" .
-docker tag "$REPOSITORY" "${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
-docker push "${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
+docker tag "$REPOSITORY" "${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
+docker push "${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
