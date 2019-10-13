08a909e6fbc2db6bb7242552807a8c4270c5e925
Joe Sweeney <joe@j1.io>
2019-10-13 18:34:55 -0400

    Simplify Docker push script

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+DOCKER_IMAGE_NAME="${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build --tag "$REPOSITORY" .
-docker tag "$REPOSITORY" "${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
-docker push "${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
+docker build --tag "$DOCKER_IMAGE_NAME" .
+docker push "$DOCKER_IMAGE_NAME"
