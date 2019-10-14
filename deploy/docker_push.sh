c2371ada5c08ac2e82b4749ab0788b55d7251304
Joe Sweeney <joe@j1.io>
2019-10-14 01:43:36 -0400

    Better handle Git to Docker tag translation

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+if [ -z "$TRAVIS_TAG" ]
+then
+  DOCKER_IMAGE_TAG="$TRAVIS_TAG"
+elif [ "$TRAVIS_BRANCH" = 'master' ]
+then
+  DOCKER_IMAGE_TAG='latest'
+else
+  echo 'Could not find a Git tag or an appropriate branch to use for the Docker image tag!'
+  exit 1
+fi
+
+DOCKER_IMAGE_NAME="${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build --tag "$DOCKER_IMAGE_NAME" .
+docker build --tag "$REPOSITORY" .
+docker tag "$REPOSITORY" "$DOCKER_IMAGE_NAME"
 docker push "$DOCKER_IMAGE_NAME"
