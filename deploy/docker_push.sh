697b4a64b7f23d85f235527fd2bc93acd59dfb02
Joe Sweeney <joe@j1.io>
2019-10-14 01:52:10 -0400

    Fix Docker push script

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -z "$TRAVIS_TAG" ]
-then
-  DOCKER_IMAGE_TAG="$TRAVIS_TAG"
-elif [ "$TRAVIS_BRANCH" = 'master' ]
+if [ "$TRAVIS_BRANCH" = 'master' ]
 then
   DOCKER_IMAGE_TAG='latest'
 else
-  echo 'Could not find a Git tag or an appropriate branch to use for the Docker image tag!'
-  exit 1
+  DOCKER_IMAGE_TAG="$TRAVIS_BRANCH"
 fi
 
 DOCKER_IMAGE_NAME="${DOCKER_USERNAME}"/"${REPOSITORY}":"${DOCKER_IMAGE_TAG}"
