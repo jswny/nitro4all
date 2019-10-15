ee3fcda5f88782006d605cc9dac4449b1d7a7cb2
Joe Sweeney <joe@j1.io>
2019-10-14 22:13:11 -0400

    Fix SSH setup in Travis

--- deploy/setup_ssh.sh
+++ deploy/setup_ssh.sh
@@ -1,5 +1,5 @@
-openssl aes-256-cbc -K "$encrypted_200a04de1233_key" -iv "$encrypted_200a04de1233_iv" -in "${DEPLOY_DIR}"/deploy_key.enc -out "${DEPLOY_DIR}"/deploy_key -d
+openssl aes-256-cbc -K "$encrypted_200a04de1233_key" -iv "$encrypted_200a04de1233_iv" -in "${TRAVIS_BUILD_DIR}"/"${DEPLOY_DIR}"/deploy_key.enc -out "${TRAVIS_BUILD_DIR}"/"${DEPLOY_DIR}"/deploy_key -d
 eval "$(ssh-agent -s)"
-chmod 600 "${DEPLOY_DIR}"/deploy_key
+chmod 600 "${TRAVIS_BUILD_DIR}"/"${DEPLOY_DIR}"/deploy_key
 ssh-add "${DEPLOY_DIR}"/deploy_key
 ssh-keyscan -t "$TRAVIS_SSH_KEY_TYPES" -H "$DEPLOY_SERVER" 2>&1 >> "${TRAVIS_HOME}"/.ssh/known_hosts
