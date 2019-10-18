e3b044a414645dc844da1e7fbc3312775b54d7eb
Joe Sweeney <joe@j1.io>
2019-10-18 19:19:41 -0400

    Revert "Add initial epic users impl"
    This reverts commit 8966c83732b8ac69178d89287f934977ee5bebf3.

--- src/main/java/io/j1/nitro4all/bot/Runner.java
+++ src/main/java/io/j1/nitro4all/bot/Runner.java
@@ -5,19 +5,17 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.*;
-import discord4j.core.object.util.Snowflake;
+import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.object.util.Permission;
 import io.j1.nitro4all.domain.MessageEmoijTuple;
 import io.j1.nitro4all.domain.MessageExt;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-import java.lang.Math;
+import java.util.function.BooleanSupplier;
 
 public class Runner {
     public static void main(String[] args) {
@@ -51,52 +49,7 @@ public class Runner {
                 .filter(msg -> !state.getNitroCheck() || !MessageExt.isMemberNitro(msg))
                 .map(MessageEmoijTuple::build)
                 .filter(MessageEmoijTuple::doesMessageContainNitroEmoji)
-                .flatMap(MessageEmoijTuple::reactWithNitroEmojisFromMessage)
-                .subscribe();
-
-        String addName = "epics";
-
-        client.getEventDispatcher().on(MessageCreateEvent.class)
-                .map(MessageCreateEvent::getMessage)
-                .filter(MessageExt::isInGuild)
-                .filter(msg -> state.getEnabled())
-                .filter(MessageExt::isMemberAdmin)
-                .filter(msg -> msg.getContent().map(content -> content.startsWith("/n4a " + addName + " add")).orElse(false))
-                .map(msg -> Tuples.of(msg, msg.getUserMentions()))
-                .map(tuple -> {
-                    Snowflake guildId = tuple.getT1().getGuild().map(Guild::getId).block();
-                    Flux<Member> memberFlux = tuple.getT2().flatMap(user -> user.asMember(guildId));
-                    return Tuples.of(tuple.getT1(), memberFlux);
-                })
-                .map(tuple -> {
-                    state.addAllEpics(tuple.getT2().map(Member::getId).collect(Collectors.toSet()).block());
-                    return tuple;
-                })
-                .map(tuple -> tuple.mapT1(Message::getChannel))
-                .flatMap(tuple -> tuple.getT1().flatMap(channel -> channel.createMessage("Nitro4All added epic users: " + tuple.getT2().map(Member::getNicknameMention).collect(Collectors.joining(", ")).block())))
-                .subscribe();
-
-        client.getEventDispatcher().on(MessageCreateEvent.class)
-                .map(MessageCreateEvent::getMessage)
-                .filter(MessageExt::isInGuild)
-                .filter(MessageExt::isMemberAdmin)
-                .filter(msg -> msg.getContent().map(("/n4a " + addName + " get")::equals).orElse(false))
-                .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Nitro4All epic users: " + state.getEpics().stream().map(Snowflake::toString).collect(Collectors.joining(", "))))
-                .subscribe();
-
-        client.getEventDispatcher().on(MessageCreateEvent.class)
-                .map(MessageCreateEvent::getMessage)
-                .filter(MessageExt::isInGuild)
-                .filter(message -> message.getAuthor().map(author -> state.isEpic(author.getId())).orElse(false))
-                .map(MessageEmoijTuple::build)
-                .flatMap(tuple -> {
-                    List<GuildEmoji> emojiList = tuple.getT2().collect(Collectors.toList()).block();
-                    Collections.shuffle(emojiList);
-                    List<GuildEmoji> subItems = emojiList.subList(0, 10);
-                    Flux<GuildEmoji> emojis = Flux.fromIterable(subItems);
-                    return MessageEmoijTuple.reactWithEmojis(Tuples.of(tuple.getT1(), emojis));
-                })
+                .flatMap(MessageEmoijTuple::reactWithNitroEmojis)
                 .subscribe();
 
         Handler.registerStateToggleHandler(client, "enabled", state::toggleEnabled);
--- src/main/java/io/j1/nitro4all/bot/State.java
+++ src/main/java/io/j1/nitro4all/bot/State.java
@@ -1,28 +1,8 @@
 package io.j1.nitro4all.bot;
 
-import discord4j.core.object.util.Snowflake;
-
-import java.util.HashSet;
-import java.util.Set;
-
 public class State {
     private boolean nitroCheck = true;
     private boolean enabled = true;
-    private Set<Snowflake> epics = new HashSet<>();
-
-    public boolean addAllEpics(Set<Snowflake> snowflakeSet) {
-        return epics.addAll(snowflakeSet);
-    }
-
-    public boolean removeEpic(Snowflake snowflake) {
-        return epics.remove(snowflake);
-    }
-
-    public boolean isEpic(Snowflake snowflake) {
-        return epics.contains(snowflake);
-    }
-
-    public Set<Snowflake> getEpics() { return epics; }
 
     public boolean toggleNitroCheck() {
         nitroCheck = !nitroCheck;
